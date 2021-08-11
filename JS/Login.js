28e5fe38674c0d087b0298498b29874f54670894
biiiiiin <ksfan2003@gmail.com>
2021-08-11 16:25:08 +0900

    login edit

--- JS/Login.js
+++ JS/Login.js
@@ -1,6 +1,7 @@
 function login() {
     const user_email = document.getElementById("login_email").value;
     const user_pwd = document.getElementById("login_password").value;
+    const login = document.querySelector("#Login_Logout");
 
     axios.post('http://52.78.200.49:3000/login',{
         "email" : "yunx2@naver.com",
@@ -9,11 +10,13 @@ function login() {
     .then((response)=>{
         console.log(response);
         alert('로그인 되었습니다.');
-        history.back();
+        location.href="/Page/Main_Page.html";
     })
     .catch((error)=>{
         console.log(error);
+        login.innerText="로그아웃";
         alert('다시 로그인 해주세요.');
+        location.href="/Page/Main_Page.html";
     })
 }
 
@@ -30,7 +33,7 @@ function register(){
     .then((response)=>{
         console.log(response);
         alert("회원가입이 완료되었습니다.");
-        history.back();
+        location.href="/Page/Login.html";
     })
     .catch((error)=>{
         console.log(error);
